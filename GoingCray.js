0924043f97b86be5cbc5f010926aa4727ef4b459
HomateJoran <158488912+HomateJoran@users.noreply.github.com>
2024-02-04 12:12:05 +0100

    Update GoingCray.js

--- GoingCray.js
+++ GoingCray.js
@@ -1,15 +1,12 @@
-<script>
-    // Set a default value for the hidden field
     document.addEventListener('DOMContentLoaded', function () {
-        document.querySelector('input[name="PlaatsHF"]').value = 'yeet cowboy';
-    });
+        var hiddenField = document.querySelector('input[name="PlaatsHF"]');
+        var radioButtons = document.querySelectorAll('input[name="Plaats"]');
 
-    // Attach an event listener to the radio buttons
-    var radioButtons = document.querySelectorAll('input[name="Plaats"]');
-    radioButtons.forEach(function (radioButton) {
-        radioButton.addEventListener('change', function () {
-            // Update the hidden field value when a radio button is selected
-            document.querySelector('input[name="PlaatsHF"]').value = this.value;
+        hiddenField.value = 'yeet cowboy';
+
+        radioButtons.forEach(function (radioButton) {
+            radioButton.addEventListener('change', function () {
+                hiddenField.value = this.value;
+            });
         });
     });
-</script>
